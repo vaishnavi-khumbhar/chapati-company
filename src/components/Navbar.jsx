4ec2bdbd4cd7b64e13cda8a74db4d9524cb0a0c8
Vaishnavi Kumbhar <kvaish1999u@gmail.com>
2026-05-28 15:45:55 +0530

    fixed mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
 import { FaBars, FaShoppingCart } from "react-icons/fa";
 
 function Navbar() {
+
+  // CLOSE MOBILE NAVBAR
+  const closeNavbar = () => {
+    const nav = document.getElementById("navMenu");
+
+    if (nav.classList.contains("show")) {
+      const bsCollapse = new window.bootstrap.Collapse(nav, {
+        toggle: false,
+      });
+
+      bsCollapse.hide();
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg fixed-top custom-navbar">
 
@@ -9,15 +23,8 @@ function Navbar() {
         {/* LOGO */}
 
         <a className="navbar-brand logo-text" href="#home">
-
-          <span className="logo-yellow">
-            The Chapati
-          </span>
-
-          <span className="logo-white">
-            {" "}Company
-          </span>
-
+          <span className="logo-yellow">The Chapati</span>
+          <span className="logo-white"> Company</span>
         </a>
 
         {/* TOGGLER */}
@@ -38,60 +45,48 @@ function Navbar() {
           <ul className="navbar-nav mx-auto align-items-lg-center">
 
             <li className="nav-item">
-              <a className="nav-link active-link" href="#home">
+              <a className="nav-link active-link" href="#home" onClick={closeNavbar}>
                 Home
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#about">
+              <a className="nav-link" href="#about" onClick={closeNavbar}>
                 About
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#features">
+              <a className="nav-link" href="#features" onClick={closeNavbar}>
                 Features
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#services">
+              <a className="nav-link" href="#services" onClick={closeNavbar}>
                 Services
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#gallery">
+              <a className="nav-link" href="#gallery" onClick={closeNavbar}>
                 Gallery
               </a>
             </li>
 
-            {/* FIXED TESTIMONIALS */}
-
             <li className="nav-item">
-              <a className="nav-link" href="#testimonials">
+              <a className="nav-link" href="#testimonials" onClick={closeNavbar}>
                 Testimonials
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#contact">
+              <a className="nav-link" href="#contact" onClick={closeNavbar}>
                 Contact
               </a>
             </li>
 
           </ul>
-
-          {/* BUTTON */}
-
-         <button className="btn order-btn">
-
-  <FaShoppingCart />
-
-  <span>Order Now</span>
-
-</button>
 
         </div>
 
